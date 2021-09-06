d9c085e39d5921898f9f848db10cfb46027da109
Tejas <tejassinghsital@gmail.com>
2021-09-06 22:05:48 +0530

    Today's practice of java

---
+++ P2Powersum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class P2Powersum
+{
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter base value: ");
+		int x= sc.nextInt();
+		System.out.println("Enter power value: ");
+		int n=sc.nextInt();
+
+		
+	}
+}
